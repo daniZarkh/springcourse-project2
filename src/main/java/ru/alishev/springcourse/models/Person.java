2dbf2e7b2b56b4ecbafb8711034509b9bacfb146
noslav7 <noslav7@gmail.com>
2023-03-15 16:36:39 +0500

    Initial commit

---
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -0,0 +1,48 @@
+package ru.alishev.springcourse.models;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    private String fullName;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    private int yearOfBirth;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
